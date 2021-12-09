b9c13c9d2e1277596b3073151fd139ba79d3ef55
benibenisan <72596397+benibenisan@users.noreply.github.com>
2021-12-09 11:12:36 +0900

    aaaaa

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+	<title>ちょっくら貯金(仮)</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+  <div class="s">
+    <h1>ちょっくら貯金(仮)</h1>
+  <div class="btn">
+    <a href="moku.php">貯金する</a>
+  </div>
+</body>
+</html>
